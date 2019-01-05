ab475d9039b7a997b9a6f90b1b9f9752b524ca1e
JoselynDRF <mariajoselyndosramos@yahoo.es>
2019-01-05 14:42:40 +0000

    Update dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -14,8 +14,8 @@ module.exports = {
   },
 
   module: {
-    loaders: [
-      { test: /(\.js|jsx)$/, exclude: /node_modules/, loaders: ['babel-loader'] },
+    rules: [
+      { test: /(\.js|jsx)$/, exclude: /node_modules/, loader: 'babel-loader' },
       { test: /\.scss/, loader: 'style-loader!css-loader!sass-loader' },
       { test: /\.(png|jpg|gif|ico)$/, loader: 'file-loader', options: { name: 'img/[name].[ext]' } },
     ],
